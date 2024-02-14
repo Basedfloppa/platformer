14399137d60d54c6aef87395a27a2cd6b54165df
Basedfloppa <zorissimoxxl@mail.ru>
2024-02-14 18:03:31 +0300

    the files

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,18 +1,21 @@
 extends CharacterBody3D
 
-@export var base_spd = 5.0
+@export var base_spd = 10.0
+@export var run_spd = 20.0
+@export var max_spd = 100.0
 @export var base_fov = 80.0
-@export var run_spd = 7.0
 @export var mouse_sensivity = 0.1
 
-const JUMP_VELOCITY = 4
+const JUMP_VELOCITY = 15
 const max_double_jump = 1
-const gravity = 9.8
+const gravity = 10
 const deceleration = 2
 
 var spd = base_spd
 var on_wall = false
 var double_jump = 1
+var time_in_jump = 0
+var starting_position = Vector3.ZERO
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -23,7 +26,7 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		$Camera.rotation_degrees.x -= event.relative.y * mouse_sensivity
 		$Camera.rotation_degrees.x = clamp($Camera.rotation_degrees.x, -90, 90)
-		rotation_degrees.y -= event.relative.x * mouse_sensivity
+		rotation_degrees.y -= event.relative.x * mouse_sensivity * clamp( 1 - (spd / base_spd / max_spd) if spd > base_spd else 1.0, 0.5, 1)
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit() 
 
@@ -44,17 +47,23 @@ func _on_area_body_entered(body):
 func movement(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta * (1.5 if velocity.y > 0 else 1.0)
+		if velocity.y > 0 : time_in_jump += delta
+		# velocity.y -= gravity * delta * ( 1 + time_in_air )
+		velocity.y -= (gravity * time_in_jump * global_position.distance_to(starting_position)) * delta
 	else:
 		double_jump = max_double_jump
 
+
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or double_jump >= 1):
+		time_in_jump = 0
+		starting_position = global_position
 		velocity.y = JUMP_VELOCITY
-		if not is_on_floor() and double_jump >= 1: double_jump -= 1
+		if not is_on_floor():
+			if double_jump >= 1: double_jump -= 1
 	if Input.is_action_pressed("run"):
-		$Camera.fov = lerpf($Camera.fov, base_fov + 10, 0.1)
-		spd = clamp(spd + (2 * delta),base_spd,100)
+		$Camera.fov = lerpf($Camera.fov, base_fov + spd, 0.1)
+		spd = clamp(spd + (2 * delta),base_spd,max_spd)
 	else:
 		$Camera.fov = lerpf($Camera.fov, base_fov, 0.2)
 		spd = lerpf(spd, base_spd, 0.1)
@@ -71,11 +80,11 @@ func movement(delta):
 	var camera_vec3 = $Camera.get_global_transform().basis.z	
 	var camera_2d = Vector2(camera_vec3.x,camera_vec3.z)
 	var velocity_2d = Vector2(velocity.x,velocity.z)
-	var ang = abs(rad_to_deg(camera_2d.angle_to(velocity_2d)))
+	var ang = abs(floor(rad_to_deg(camera_2d.angle_to(velocity_2d))))
 	if ang <= 90 and ang >= 46:
-		spd = spd * 0.7
+		spd = spd * 0.8
 	elif ang >= 0 and ang <= 45:
-		spd = spd * 0.5
+		spd = spd * 0.7
 	
 	move_and_slide()
 	
